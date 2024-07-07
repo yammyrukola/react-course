9b270c100e841cd25611dcddca68e8c3d27e993c
gitalexus <ru9.alex@gmail.com>
2024-07-07 22:16:48 +0300

    chore: initial project setting

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'react', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 'off',  // Отключение правила, требующего React в области видимости для JSX
+    '@typescript-eslint/no-explicit-any': "error", // Включение явной проверки на наличие any
+    'prettier/prettier': 'error',
+    'prettier/prettier': ['error', { 'singleQuote': true }],
+  },
+}
